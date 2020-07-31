cff2af97da207feb76d59a9bdd0e2566d0166c46
Benjamin Wiessneth <b.wiessneth@googlemail.com>
2020-07-31 20:01:43 +0200

    Introducing CLI testing

---
+++ test/gaspy_test.py
@@ -0,0 +1,73 @@
+import subprocess
+import os
+import hashlib
+
+def capture(command):
+    proc = subprocess.Popen(command,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+    )
+    out,err = proc.communicate()
+    return out, err, proc.returncode
+
+
+def generate_md5(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+        return hashlib.md5(data).hexdigest()
+    return -1
+
+
+def test_gaspy_cli_arg_help():
+    command = ["gaspy", "-h"]
+    out, err, exitcode = capture(command)
+    assert exitcode == 0    
+
+    command = ["gaspy", "--help"]
+    out, err, exitcode = capture(command)
+    assert exitcode == 0    
+
+
+def test_gaspy_cli_arg_version():
+    command = ["gaspy", "-v"]
+    out, err, exitcode = capture(command)
+    assert exitcode == 0    
+    assert out == b'gaspy 1.0.0\r\n'
+    assert err == b''    
+
+    command = ["gaspy", "--version"]
+    out, err, exitcode = capture(command)
+    assert exitcode == 0    
+    assert out == b'gaspy 1.0.0\r\n'
+    assert err == b''
+
+
+def test_gaspy_cli_objectcode_comparison_static():
+    source_directory = "test/asm_source/"
+    output_directory = "test/gaspy_output/"
+    gasm_directory = "test/gasm_output/"
+    sample_sources = [
+        "sample.gsm",
+        "sensor_isr.gsm",
+        "polling_loop.gsm",
+        "rtc_handler.gsm",
+        "jmp.gsm",
+        "bz_bnz.gsm",
+        "bc_bnc.gsm",
+        "ldm.gsm",
+    ]
+
+    for source in sample_sources:
+        source_name, source_ext = os.path.splitext(source)
+        datafile = os.path.join(output_directory, source_name + "_data.dat")
+        textfile = os.path.join(output_directory, source_name + "_text.dat")
+        gasm_datafile = os.path.join(gasm_directory, source_name + "_data.dat")
+        gasm_textfile = os.path.join(gasm_directory, source_name + "_text.dat")
+
+        command = ["gaspy", source_directory + source, "-o", output_directory]
+        out, err, exitcode = capture(command)
+        assert exitcode == 0    
+
+        # Create md5 hash and compare outputs
+        assert generate_md5(textfile) == generate_md5(gasm_textfile)
+        assert generate_md5(datafile) == generate_md5(gasm_datafile)
